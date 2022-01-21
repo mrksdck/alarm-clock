d25bf2c07a81ae46f189f7f21a9b6909adb74f7a
Markus Decke <mrksdck@users.noreply.github.com>
2022-01-21 23:33:15 +0100

    alarm clock passing requirement
    requirement is that it should ring when it is six o'clock

---
+++ src/main/kotlin/alarm.kt
@@ -0,0 +1,16 @@
+import java.time.LocalTime
+
+/*
+This is a code example of an alarm clock
+It sufficiently covers the requirement that it should ring at six o'clock
+ */
+
+class alarm {
+
+    fun ring(){
+        val time = LocalTime.now()
+        if(time.equals(LocalTime.of(6,0))){
+            print("It is 6:00 o'clock")
+        }
+    }
+}
