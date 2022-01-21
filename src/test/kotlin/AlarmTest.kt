f41aa9d2247ba3b385b4277ad3afa92959cefd87
Markus Decke <mrksdck@users.noreply.github.com>
2022-01-22 00:41:21 +0100

    adds spy to alarm test to verfiy it works

--- src/test/kotlin/AlarmTest.kt
+++ src/test/kotlin/AlarmTest.kt
@@ -1,11 +1,16 @@
 import io.kotest.core.spec.style.FunSpec
+import io.mockk.confirmVerified
+import io.mockk.spyk
+import io.mockk.verify
 import java.time.LocalTime
 
 class AlarmTest : FunSpec(){
     init {
         test("alarm clock rings") {
-            val alarm = Alarm()
+            val alarm = spyk(Alarm())
             alarm.ring(LocalTime.of(6,0))
+            verify { alarm.printRing()}
+
         }
     }
 }
