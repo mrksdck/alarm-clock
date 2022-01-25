e04c0193627589b795195b59de23fa07b4e35c2d
Markus Decke <mrksdck@users.noreply.github.com>
2022-01-25 18:39:55 +0100

    verify all calls on spy

--- src/test/kotlin/AlarmTest.kt
+++ src/test/kotlin/AlarmTest.kt
@@ -1,4 +1,5 @@
 import io.kotest.core.spec.style.FunSpec
+import io.kotest.matchers.ints.exactly
 import io.mockk.confirmVerified
 import io.mockk.spyk
 import io.mockk.verify
@@ -9,8 +10,11 @@ class AlarmTest : FunSpec(){
         test("alarm clock rings") {
             val alarm = spyk(Alarm())
             alarm.ring(LocalTime.of(6,0))
-            verify { alarm.printRing()}
 
+            verify { alarm.ring(LocalTime.of(6,0)) }
+            verify {  alarm.printRing()}
+
+            confirmVerified(alarm.printRing())
         }
     }
 }
